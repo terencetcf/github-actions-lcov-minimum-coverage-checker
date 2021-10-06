73c13b771a0f991070ec310017a2dcc06516c07a
Terence Tai <terencetcf@hotmail.com>
2021-10-06 11:34:06 +0800

    update build setup

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,37 @@
 import fs from 'fs';
-import path from 'path';
-import os from 'os';
-import core from '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
 import parseLCOV from 'parse-lcov';
 
-async function run() {
+async function run(): Promise<void> {
   try {
-    core.info(`Start...`);
-    const tmpPath = path.resolve(os.tmpdir(), github.context.action);
     const coverageFilesPattern = core.getInput('coverage-file');
-    const coverageFilePath = tmpPath + coverageFilesPattern;
-
+    const coverageFilePath = `./${coverageFilesPattern}`;
     core.info(`Reading coverage file (${coverageFilePath}) content...`);
-    // const coverageFileContent = fs.readFileSync(coverageFilePath, {
-    //   encoding: 'utf-8',
-    // });
-
-    // core.info(`Parsing lcov results...`);
-    // const lcovRecords = parseLCOV(coverageFileContent);
-    // const totalLinesHit = lcovRecords.reduce<number>(
-    //   (acc, rec) => acc + rec.lines.hit,
-    //   0
-    // );
-    // const totalLinesFound = lcovRecords.reduce<number>(
-    //   (acc, rec) => acc + rec.lines.found,
-    //   0
-    // );
-    // const totalCoverage = Math.round((totalLinesHit / totalLinesFound) * 100);
-    // const minimumCoverage = parseInt(core.getInput('minimum-coverage'));
-    // const isFailure = totalCoverage < minimumCoverage;
-
-    // if (isFailure) {
-    //   core.setFailed(
-    //     `The current code coverage (${totalCoverage}%) is below the ${minimumCoverage}%.`
-    //   );
-    //   return;
-    // }
-
-    // core.info(`The current code coverage ${totalCoverage}%`);
+    const coverageFileContent = fs.readFileSync(coverageFilePath, {
+      encoding: 'utf-8',
+    });
+    core.info(`Parsing lcov results...`);
+    const lcovRecords = parseLCOV(coverageFileContent);
+    const totalLinesHit = lcovRecords.reduce<number>(
+      (acc, rec) => acc + rec.lines.hit,
+      0
+    );
+    const totalLinesFound = lcovRecords.reduce<number>(
+      (acc, rec) => acc + rec.lines.found,
+      0
+    );
+    const totalCoverage = Math.round((totalLinesHit / totalLinesFound) * 100);
+    const minimumCoverage = parseInt(core.getInput('minimum-coverage'));
+    const isFailure = totalCoverage < minimumCoverage;
+    if (isFailure) {
+      core.setFailed(
+        `The current code coverage (${totalCoverage}%) is below the ${minimumCoverage}%.`
+      );
+      return;
+    }
+    core.info(`The current code coverage ${totalCoverage}%`);
   } catch (error: any) {
-    // core.setFailed(error);
+    core.setFailed(error);
   }
 }
 
